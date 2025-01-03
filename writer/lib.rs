c877a6b649e6138c34a51f1205bee7c316ba31f6
aplefull <aplefull@gmail.com>
2025-01-03 15:03:35 +0100

    remove unused dependency

--- writer/lib.rs
+++ writer/lib.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{Write, Error};
+use std::io::{Error, Write};
 use std::path::{Path, PathBuf};
 use webp::{AnimEncoder, AnimFrame, Encoder, WebPConfig};
 
@@ -43,16 +43,19 @@ impl Writer {
         let frames = &image.frames;
 
         let mut timestamp_ms = 0i32;
-        let anim_frames = frames.iter().map(|frame| {
-            let delta = if frame.delay == 0 { 1 } else { frame.delay as i32 };
-            timestamp_ms = timestamp_ms + delta;
+        let anim_frames = frames
+            .iter()
+            .map(|frame| {
+                let delta = if frame.delay == 0 { 1 } else { frame.delay as i32 };
+                timestamp_ms = timestamp_ms + delta;
 
-            if frame.has_alpha {
-                AnimFrame::from_rgba(frame.pixels.as_slice(), width, height, timestamp_ms)
-            } else {
-                AnimFrame::from_rgb(frame.pixels.as_slice(), width, height, timestamp_ms)
-            }
-        }).collect::<Vec<AnimFrame>>();
+                if frame.has_alpha {
+                    AnimFrame::from_rgba(frame.pixels.as_slice(), width, height, timestamp_ms)
+                } else {
+                    AnimFrame::from_rgb(frame.pixels.as_slice(), width, height, timestamp_ms)
+                }
+            })
+            .collect::<Vec<AnimFrame>>();
 
         let mut config = WebPConfig::new().unwrap();
         config.lossless = 1i32;
@@ -96,104 +99,109 @@ impl Writer {
         Ok(())
     }
 
-     pub fn write_ppm(output_path: &PathBuf, image: &WriterImage) -> Result<(), Error> {
-         Writer::validate_pixel_count(&image)?;
+    pub fn write_ppm(output_path: &PathBuf, image: &WriterImage) -> Result<(), Error> {
+        Writer::validate_pixel_count(&image)?;
 
-         let mut file = File::create(output_path)?;
-         let width = image.frames[0].width;
-         let height = image.frames[0].height;
-         let pixels = &image.frames[0].pixels;
-         let has_alpha = image.frames[0].has_alpha;
+        let mut file = File::create(output_path)?;
+        let width = image.frames[0].width;
+        let height = image.frames[0].height;
+        let pixels = &image.frames[0].pixels;
+        let has_alpha = image.frames[0].has_alpha;
 
-         file.write_all(b"P6\n")?;
-         file.write_all(format!("{} {}\n", width, height).as_bytes())?;
-         file.write_all(b"255\n")?;
+        file.write_all(b"P6\n")?;
+        file.write_all(format!("{} {}\n", width, height).as_bytes())?;
+        file.write_all(b"255\n")?;
 
-         for y in 0..height {
-             for x in 0..width {
-                 let pixel_index = ((y * width + x) * if has_alpha { 4 } else { 3 }) as usize;
+        for y in 0..height {
+            for x in 0..width {
+                let pixel_index = ((y * width + x) * if has_alpha { 4 } else { 3 }) as usize;
 
-                 let r = pixels[pixel_index];
-                 let g = pixels[pixel_index + 1];
-                 let b = pixels[pixel_index + 2];
+                let r = pixels[pixel_index];
+                let g = pixels[pixel_index + 1];
+                let b = pixels[pixel_index + 2];
 
-                 file.write_all(&[r, g, b])?;
-             }
-         }
+                file.write_all(&[r, g, b])?;
+            }
+        }
 
-         Ok(())
-     }
+        Ok(())
+    }
 
-        pub fn write_pam(output_path: &PathBuf, width: u32, height: u32, pixel_data: &WriterPixelData) -> Result<(), Error> {
-            let mut file = File::create(output_path)?;
+    pub fn write_pam(
+        output_path: &PathBuf,
+        width: u32,
+        height: u32,
+        pixel_data: &WriterPixelData,
+    ) -> Result<(), Error> {
+        let mut file = File::create(output_path)?;
 
-            file.write_all(b"P7\n")?;
-            file.write_all(format!("WIDTH {}\n", width).as_bytes())?;
-            file.write_all(format!("HEIGHT {}\n", height).as_bytes())?;
+        file.write_all(b"P7\n")?;
+        file.write_all(format!("WIDTH {}\n", width).as_bytes())?;
+        file.write_all(format!("HEIGHT {}\n", height).as_bytes())?;
 
-            match pixel_data {
-                WriterPixelData::RGB8(pixels) => {
-                    file.write_all(b"DEPTH 3\nMAXVAL 255\nTUPLTYPE RGB\nENDHDR\n")?;
-                    file.write_all(pixels)?;
-                }
-                WriterPixelData::RGBA8(pixels) => {
-                    file.write_all(b"DEPTH 4\nMAXVAL 255\nTUPLTYPE RGB_ALPHA\nENDHDR\n")?;
-                    file.write_all(pixels)?;
-                }
-                WriterPixelData::RGB16(pixels) => {
-                    file.write_all(b"DEPTH 3\nMAXVAL 65535\nTUPLTYPE RGB\nENDHDR\n")?;
-                    for value in pixels {
-                        file.write_all(&value.to_be_bytes())?;
-                    }
-                }
-                WriterPixelData::RGBA16(pixels) => {
-                    file.write_all(b"DEPTH 4\nMAXVAL 65535\nTUPLTYPE RGB_ALPHA\nENDHDR\n")?;
-                    for value in pixels {
-                        file.write_all(&value.to_be_bytes())?;
-                    }
-                }
-                WriterPixelData::RGB32F(pixels) => {
-                    file.write_all(b"DEPTH 3\nMAXVAL 65535\nTUPLTYPE RGB\nENDHDR\n")?;
-                    for &value in pixels {
-                        let value16 = (value.clamp(0.0, 1.0) * 65535.0) as u16;
-                        file.write_all(&value16.to_be_bytes())?;
-                    }
-                }
-                WriterPixelData::RGBA32F(pixels) => {
-                    file.write_all(b"DEPTH 4\nMAXVAL 65535\nTUPLTYPE RGB_ALPHA\nENDHDR\n")?;
-                    for &value in pixels {
-                        let value16 = (value.clamp(0.0, 1.0) * 65535.0) as u16;
-                        file.write_all(&value16.to_be_bytes())?;
-                    }
-                }
-                WriterPixelData::L1(pixels) => {
-                    file.write_all(b"DEPTH 1\nMAXVAL 1\nTUPLTYPE GRAYSCALE\nENDHDR\n")?;
-                    file.write_all(pixels)?;
-                }
-                WriterPixelData::L8(pixels) => {
-                    file.write_all(b"DEPTH 1\nMAXVAL 255\nTUPLTYPE GRAYSCALE\nENDHDR\n")?;
-                    file.write_all(pixels)?;
-                }
-                WriterPixelData::L16(pixels) => {
-                    file.write_all(b"DEPTH 1\nMAXVAL 65535\nTUPLTYPE GRAYSCALE\nENDHDR\n")?;
-                    for value in pixels {
-                        file.write_all(&value.to_be_bytes())?;
-                    }
-                }
-                WriterPixelData::LA8(pixels) => {
-                    file.write_all(b"DEPTH 2\nMAXVAL 255\nTUPLTYPE GRAYSCALE_ALPHA\nENDHDR\n")?;
-                    file.write_all(pixels)?;
-                }
-                WriterPixelData::LA16(pixels) => {
-                    file.write_all(b"DEPTH 2\nMAXVAL 65535\nTUPLTYPE GRAYSCALE_ALPHA\nENDHDR\n")?;
-                    for value in pixels {
-                        file.write_all(&value.to_be_bytes())?;
-                    }
+        match pixel_data {
+            WriterPixelData::RGB8(pixels) => {
+                file.write_all(b"DEPTH 3\nMAXVAL 255\nTUPLTYPE RGB\nENDHDR\n")?;
+                file.write_all(pixels)?;
+            }
+            WriterPixelData::RGBA8(pixels) => {
+                file.write_all(b"DEPTH 4\nMAXVAL 255\nTUPLTYPE RGB_ALPHA\nENDHDR\n")?;
+                file.write_all(pixels)?;
+            }
+            WriterPixelData::RGB16(pixels) => {
+                file.write_all(b"DEPTH 3\nMAXVAL 65535\nTUPLTYPE RGB\nENDHDR\n")?;
+                for value in pixels {
+                    file.write_all(&value.to_be_bytes())?;
                 }
             }
-
-            Ok(())
+            WriterPixelData::RGBA16(pixels) => {
+                file.write_all(b"DEPTH 4\nMAXVAL 65535\nTUPLTYPE RGB_ALPHA\nENDHDR\n")?;
+                for value in pixels {
+                    file.write_all(&value.to_be_bytes())?;
+                }
+            }
+            WriterPixelData::RGB32F(pixels) => {
+                file.write_all(b"DEPTH 3\nMAXVAL 65535\nTUPLTYPE RGB\nENDHDR\n")?;
+                for &value in pixels {
+                    let value16 = (value.clamp(0.0, 1.0) * 65535.0) as u16;
+                    file.write_all(&value16.to_be_bytes())?;
+                }
+            }
+            WriterPixelData::RGBA32F(pixels) => {
+                file.write_all(b"DEPTH 4\nMAXVAL 65535\nTUPLTYPE RGB_ALPHA\nENDHDR\n")?;
+                for &value in pixels {
+                    let value16 = (value.clamp(0.0, 1.0) * 65535.0) as u16;
+                    file.write_all(&value16.to_be_bytes())?;
+                }
+            }
+            WriterPixelData::L1(pixels) => {
+                file.write_all(b"DEPTH 1\nMAXVAL 1\nTUPLTYPE GRAYSCALE\nENDHDR\n")?;
+                file.write_all(pixels)?;
+            }
+            WriterPixelData::L8(pixels) => {
+                file.write_all(b"DEPTH 1\nMAXVAL 255\nTUPLTYPE GRAYSCALE\nENDHDR\n")?;
+                file.write_all(pixels)?;
+            }
+            WriterPixelData::L16(pixels) => {
+                file.write_all(b"DEPTH 1\nMAXVAL 65535\nTUPLTYPE GRAYSCALE\nENDHDR\n")?;
+                for value in pixels {
+                    file.write_all(&value.to_be_bytes())?;
+                }
+            }
+            WriterPixelData::LA8(pixels) => {
+                file.write_all(b"DEPTH 2\nMAXVAL 255\nTUPLTYPE GRAYSCALE_ALPHA\nENDHDR\n")?;
+                file.write_all(pixels)?;
+            }
+            WriterPixelData::LA16(pixels) => {
+                file.write_all(b"DEPTH 2\nMAXVAL 65535\nTUPLTYPE GRAYSCALE_ALPHA\nENDHDR\n")?;
+                for value in pixels {
+                    file.write_all(&value.to_be_bytes())?;
+                }
+            }
         }
+
+        Ok(())
+    }
 
     fn validate_pixel_count(image: &WriterImage) -> Result<(), Error> {
         let width = image.width;
