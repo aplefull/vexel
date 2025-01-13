5a0aa42d58c8a3c00834114f643310eb7ed03b98
aplefull <aplefull@gmail.com>
2025-01-13 22:51:22 +0100

    Reformat

--- vexel/utils/marker.rs
+++ vexel/utils/marker.rs
@@ -1,6 +1,8 @@
 use std::fmt::Debug;
 
 pub trait Marker: Debug + Clone + PartialEq {
-    fn from_u16(value: u16) -> Option<Self> where Self: Sized;
+    fn from_u16(value: u16) -> Option<Self>
+    where
+        Self: Sized;
     fn to_u16(&self) -> u16;
 }
